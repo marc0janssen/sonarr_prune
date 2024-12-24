73dc403a1435dfda97bd9e1b5da98d690c1bfd0c
Marco <marco@mjanssen.nl>
2024-12-24 12:58:55 +0100

    diskinfo

--- app/sonarr_prune.py
+++ app/sonarr_prune.py
@@ -72,7 +72,7 @@ class SONARRPRUNE():
                 self.remove_after_days = int(
                     self.config['PRUNE']['REMOVE_SERIES_AFTER_DAYS'])
                 self.remove_percentage = float(
-                    self.config['PRUNE']['REMOVE_MOVIES_DISK_PERCENTAGE'])
+                    self.config['PRUNE']['REMOVE_SERIES_DISK_PERCENTAGE'])
                 self.warn_days_infront = int(
                     self.config['PRUNE']['WARN_DAYS_INFRONT'])
                 self.dry_run = True if (
